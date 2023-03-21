991f3ba50e62612d0ef6d9910043a21346e9e412
jakin6 <irijakin.com.com>
2023-03-21 18:28:01 +0100

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './home.scss'
+export const Home = () => {
+  return (
+    <div className='home'>
+        <h1 className="title">
+            React project
+        </h1>
+    </div>
+  )
+}
